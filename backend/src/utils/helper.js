a985a7a6761d929b76ac9b051bb78f1009e5e7a0
Manish <mdashsharma95@gmail.com>
2024-07-20 22:36:26 +0530

    fixed frontend URL

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -3,7 +3,7 @@ import config from "../config/index.js";
 
 const loginURL = "https://100014.pythonanywhere.com";
 // const frontend_url = "http://localhost:5173/dowelleducation/";
-const frontend_url = "www.samantaedu.uxlivinglab.online/dowelleducation";
+const frontend_url = "https://www.samantaedu.uxlivinglab.online/dowelleducation";
 const scaleURL = "https://100035.pythonanywhere.com/addons/create-scale/"
 const learningIndexScaleUrl = "https://www.uxlive.me/dowellscale/samanta-edu/"
 
