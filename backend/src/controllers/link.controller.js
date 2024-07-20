4065d455e460ed11e51f0c592113a51575c09fe8
Manish <mdashsharma95@gmail.com>
2024-07-21 03:16:03 +0530

    Delete API to delete a link

--- backend/src/controllers/link.controller.js
+++ backend/src/controllers/link.controller.js
@@ -85,5 +85,31 @@ const getLink = asyncHandler(async(req, res) => {
     })
 });
 
+const deleteLink = asyncHandler(async(req,res)=>{
+    const workspaceId = req.params.workspaceId
+    if(!workspaceId){
+        return res.status(400)
+        .json({
+            success: false,
+            message: "Please provide workspaceId"
+       })
+    }
+    const link = await Link.findOneAndDelete({ workspaceId: workspaceId });
 
-export { createLink, getLink };
+    if(!link){
+        return res.status(404)
+        .json({
+            success: false,
+            message: "Link not found"
+       })
+    }
+
+    return res.status(200)
+    .json({
+        success: true,
+        message: "Link deleted successfully"
+    })
+})
+
+
+export { createLink, getLink, deleteLink};
--- backend/src/routes/link.route.js
+++ backend/src/routes/link.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {
-    createLink,getLink
+    createLink,getLink,deleteLink
 } from "../controllers/link.controller.js";
 const router = Router();
 
 router.post("/generate-link", createLink);
 router.get("/:workspaceId", getLink);
+router.delete("/:workspaceId",deleteLink)
 
 export default router;
